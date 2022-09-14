3ba7aa334319cc1c43e822bfd644585964f61759
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-09-14 00:36:18 -0300

    Adicionando

---
+++ analisador-de-textos.py
@@ -0,0 +1,5 @@
+name = str(input('Digite seu nome completo: '))
+print('Seu nome em maiúsculas é {}'.format(name.upper()))
+print('Seu nome em minúsculas é ' + name.lower())
+print('Seu nome tem ao todo {} letras'.format(len(name) - name.count(' ')))
+print('Seu primeiro nome é {} e tem {} letras'.format(name.split()[0], len(name.split()[0])))
