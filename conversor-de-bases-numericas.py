69437db8051f4f819d9674a6ab225d296ebebfd0
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-11-06 18:45:17 -0300

    Adicionando

--- conversor-de-bases-numericas.py
+++ conversor-de-bases-numericas.py
@@ -7,4 +7,10 @@ print('''Escolha uma das bases para conversão:
 opcao = int(input('Sua opcao: '))
 
 if opcao == 1:
-    print(' ')
+    print('{} convertido para BINÁRIO é igual a {}'.format(num, bin(num)[2:]))
+elif opcao == 2:
+    print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else:
+    print('Seu número {} é inválido.'.format(num))
